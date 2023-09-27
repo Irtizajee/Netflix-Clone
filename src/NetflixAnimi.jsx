8eb0eb6f15f5bef5720b4f9f387f24be28a1765e
Irtaza <izziali123@gmail.com>
2023-09-27 20:02:45 +0500

    initial

--- src/NetflixAnimi.jsx
+++ src/NetflixAnimi.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import netflix_tv from './netflix_tv.png';
-import vid from './vid.mp4'
+import netflix_tv_img from './netflix_tv_img.jpg'
+// import vid from './vid.mp4'
 // import 'vid.mp4/dist/style.css';
-import VideoPLayer from "react-video-js-player";
+// import VideoPLayer from "./react-video-js-player";
 function NetflixAnimi() {
   return (
     <>
- <div  className='containerStyleImg'>
-    <img src={netflix_tv} alt="" >
+ <div   className='containerStyleImg'>
+    <img   src={netflix_tv} alt="" />
      
-    </img>
-    {/* <VideoPLayer src={vid} width="300px" height="300px"  /> */}
+    
+    
+   
     </div>
     </>
   )
